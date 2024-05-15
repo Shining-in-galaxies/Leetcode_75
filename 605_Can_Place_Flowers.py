bd7ae78201e4e5907dab91e1b4e5697d1974cf76
Shining-in-galaxies <januarymafia@gmail.com>
2024-05-15 14:20:11 -0700

    240515

---
+++ 605_Can_Place_Flowers.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        
+        if(n==0):
+            return True
+
+        planted = 0
+
+        for i in range(len(flowerbed)):
+            leftIsEmpty = (i == 0) or (flowerbed[i-1] == 0)
+            rightIsEmpty = (i == len(flowerbed)-1) or (flowerbed[i+1] == 0)
+
+            if(flowerbed[i] == 0 and leftIsEmpty and rightIsEmpty):
+                flowerbed[i] = 1
+                planted += 1
+
+                if(planted == n):
+                    return True
+
+        return False
